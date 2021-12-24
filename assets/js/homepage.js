cc3a1c5ce93b71ac59a3f93612c337fb36c2ec07
Zane Scheel <zane.scheel@gmail.com>
2021-12-24 08:45:15 -0600

    finished setup of function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,11 @@
 var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-    console.log("function was called");
+    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    })
+
+    console.log("outside");
 }
 
 getUserRepos();
